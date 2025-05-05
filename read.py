0b2111910d7c56c88dbc4596f7f330ba2a9b1cd7
alvarop0777 <personalalvaropineros0777@gmail.com>
2025-05-05 10:04:43 -0500

    Update read.py

--- read.py
+++ read.py
@@ -1,11 +1,9 @@
 import json
 
-# Load the JSON file
 with open('muscle_groups.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-# Print the data
-for group, exercises in data.items():
-    print(f"\nMuscle group: {group}")
-    for exercise in exercises:
-        print(f" - {exercise}")
+for group in data['muscle_groups']:
+    print(f"\nMuscle group: {group['name']}")
+    for ex in group['exercises']:
+        print(f" - {ex['name']} ({ex['type']}, {ex['equipment']}, Reps: {ex['reps_for_hypertrophy']})")
